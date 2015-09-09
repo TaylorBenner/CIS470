d94e0dfde7590b0482a2bd4873ab061c0047a16a
Taylor Benner <taylor.benner@madwiremedia.com>
2015-09-09 16:00:33 -0600

    incorporated brain

--- movement.py
+++ movement.py
@@ -1,10 +1,17 @@
+import random, math, sys
 import pygame
-import random
-import math
 from pygame.locals import *
+
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure import TanhLayer
 
 WIDTH  = 700
 HEIGHT = 700
+DEBUG  = False
+
+class Brain:
+	def __init__( self, shape ):
+		self.network = buildNetwork(shape[0], shape[1], shape[2], outclass=TanhLayer, bias=True)
 
 class Main:
 
@@ -46,6 +53,8 @@ class Main:
 
 	# update loop
 	def update( self ):
+
+		self.member.think()
 
 		if len(self.food) == 0:
 			for i in range(0,10):
@@ -124,24 +133,45 @@ class Main:
 class Member:
 	def __init__( self, num ):
 
+		# initial position, size, and x,y pos
 		self.radius	= 20
 		self.x 		= random.randint( self.radius, (WIDTH  - self.radius))
 		self.y 		= random.randint( self.radius, (HEIGHT - self.radius))
 
+		# member description
 		self.color  = (random.randint(100,255),random.randint(100,255),random.randint(100,255))
 		self.stroke = (0,0,0)
 
+		# member name for reporting purposes
 		self.name 	= "Creature #" + str(num)
+
+		# member movement speed
 		self.speed  = .5
+
+		# rotation amount from tread differential
 		self.rotation_speed = 0.01
+
+		# direction member is facing
 		self.angle  = 0.45
 
+		self.left_track = 0
+		self.right_track = 0
+
+		# member hitbox
 		self.rect = pygame.Rect((self.x-self.radius, self.y-self.radius),(self.radius*2, self.radius*2))
+
+		# member statistics
 		self.food_eaten = 0
 		self.damage_taken = 0
 
-		self.debug = False
+		# init brain
+		self.brain = Brain([2,5,2])
 
+	def think( self ):
+		self.left_track, self.right_track = self.brain.network.activate([self.angle, self.rotation_speed])
+		# print "%f  -  %f" %(self.left_track, self.right_track)
+
+	# function to wrap member to opposite side of screen
 	def check_bounds( self ):
 		if self.x > WIDTH:
 			self.x = 0
@@ -153,11 +183,13 @@ class Member:
 		elif self.y < 0:
 			self.y = HEIGHT
 
+	# function that calculates new position based on angle, speed, radius, and current position
 	def move( self, mod = 1 ):
 		self.x += mod * deltaX( self.angle, self.speed )
 		self.y += mod * deltaY( self.angle, self.speed )
 		self.rect = pygame.Rect((self.x-self.radius, self.y-self.radius),(self.radius*2, self.radius*2))
 
+	# function for drawing member and member's hitbox
 	def draw( self, display ):
 		pygame.draw.circle( display, self.color, (toFixed(self.x), toFixed(self.y)), self.radius, 0)
 		pygame.draw.circle( display, self.stroke, (toFixed(self.x), toFixed(self.y)), self.radius, 1)
@@ -165,44 +197,55 @@ class Member:
 		dy = self.y + deltaY( self.angle, self.radius )
 		pygame.draw.line( display, self.stroke, (toFixed(self.x), toFixed(self.y)), (toFixed(dx), toFixed(dy)), 1)
 
-		if self.debug:
+		if DEBUG:
 			pygame.draw.rect(display, (200,100,100), self.rect, 1)
 
 
 class Food:
 	def __init__( self, num ):
 
-		self.size = 10		
-		self.x = random.randint(self.size, (WIDTH  - self.size))
-		self.y = random.randint(self.size, (HEIGHT - self.size))
+		self.radius = 5
+		self.x = random.randint(self.radius, (WIDTH  - self.radius))
+		self.y = random.randint(self.radius, (HEIGHT - self.radius))
 
 		self.color  = (100,200,100)
-		self.stroke = (0,0,0)
+		self.stroke = (50,100,50)
 
 		self.name   = "Food Pellet #" + str(num)
-		self.rect   = pygame.Rect((self.x, self.y), (self.size, self.size)) 
+
+		# food hitbox
+		self.rect = pygame.Rect((self.x-self.radius, self.y-self.radius),(self.radius*2, self.radius*2))
 
 	def draw( self, display ):
-		pygame.draw.rect( display, self.color, self.rect, 0)
-		pygame.draw.rect( display, self.stroke, self.rect, 1)
+		pygame.draw.circle( display, self.color, (self.x, self.y), self.radius, 0)
+		pygame.draw.circle( display, self.stroke, (self.x, self.y), self.radius, 1)
+
+		if DEBUG:
+			pygame.draw.rect(display, (200,100,100), self.rect, 1)
+
 
 
 class Toxin:
 	def __init__( self, num ):
 
-		self.size = 10		
-		self.x = random.randint(self.size, (WIDTH  - self.size))
-		self.y = random.randint(self.size, (HEIGHT - self.size))
+		self.radius = 5	
+		self.x = random.randint(self.radius, (WIDTH  - self.radius))
+		self.y = random.randint(self.radius, (HEIGHT - self.radius))
 
 		self.color  = (200,100,100)
-		self.stroke = (0,0,0)
+		self.stroke = (100,50,50)
 
 		self.name   = "Toxin Pellet #" + str(num)
-		self.rect   = pygame.Rect((self.x, self.y), (self.size, self.size)) 
+
+		# toxin hitbox
+		self.rect = pygame.Rect((self.x-self.radius, self.y-self.radius),(self.radius*2, self.radius*2))
 
 	def draw( self, display ):
-		pygame.draw.rect( display, self.color, self.rect, 0)
-		pygame.draw.rect( display, self.stroke, self.rect, 1)
+		pygame.draw.circle( display, self.color, (self.x, self.y), self.radius, 0)
+		pygame.draw.circle( display, self.stroke, (self.x, self.y), self.radius, 1)
+		
+		if DEBUG:
+			pygame.draw.rect(display, (200,100,100), self.rect, 1)
 
 
 def toFixed( number ):
@@ -216,5 +259,10 @@ def deltaY( angle, number ):
 
  
 if __name__ == "__main__" :
+
+	args = sys.argv
+	if len(args) > 1:
+		DEBUG = args[1]
+
 	main_window = Main()
 	main_window.execute()
