75f82ef3fae69eb106e1b47249a6aede0a08ad1c
Taylor Benner <taylor.benner@madwiremedia.com>
2015-10-05 14:40:31 -0600

    updated tests

--- classes/member.py
+++ classes/member.py
@@ -7,16 +7,19 @@ class Member:
 	radius = configuration.MEMBER_RADIUS
 
 	def __init__( self ):
-		self.x, self.y = Helper.random_coordinates( self.radius )
-		self.color     = Helper.random_color()
-		self.brain     = Brain()
+		self.x, self.y   = Helper.random_coordinates( self.radius )
+		self.color       = Helper.random_color()
+		self.brain       = Brain()
 
-		self.alive     = True
-		self.rotation  = 0.0
+		self.alive       = True
+		self.rotation    = 0.0
+		self.speed       = 0.0
+		self.left_track  = 0.0
+		self.right_track = 0.0
 
 	def update( self ):
-		self.update_position()
 		self.update_state()
+		self.update_position()
 
 	def update_position( self ):
 		pass
--- tests/membertest.py
+++ tests/membertest.py
@@ -9,33 +9,28 @@ class MemberTest( unittest.TestCase ):
 	@staticmethod
 	def buildSuite():
 		suite = unittest.TestSuite()
-		suite.addTest(MemberTest("testShouldInitializeXCoordinate"))
-		suite.addTest(MemberTest("testShouldInitializeYCoordinate"))
-		suite.addTest(MemberTest("testShouldInitializeColor"))
+		suite.addTest(MemberTest("testShouldInitialize"))
 		return suite
 
 	def setUp( self ):
-		from project import Member
+		from classes.member import Member
 		self.member = Member()
 
-	def testShouldInitializeXCoordinate( self ):
+	def testShouldInitialize ( self ):
 		lower_limit = configuration.MEMBER_RADIUS
 		upper_limit = configuration.WIDTH - configuration.MEMBER_RADIUS
-		assert (self.member.x >= lower_limit and self.member.x <= upper_limit), 'Initialized X Value is outside the bounds of the view area'
 
-	def testShouldInitializeYCoordinate( self ):
-		lower_limit = configuration.MEMBER_RADIUS
-		upper_limit = configuration.WIDTH - configuration.MEMBER_RADIUS
-		assert (self.member.y >= lower_limit and self.member.y <= upper_limit), 'Initialized Y Value is outside the bounds of the view area'
-
-	def testShouldInitializeColor( self ):
 		color_pass  = True
 		color_index = None 
 		for i,c in enumerate(self.member.color):
 			if c < 0 or c > 255:
 				color_index = i
 				color_pass  = False
-		assert color_pass == True and color_index == None, 'Color in position %i is outside accepted RGB values' % color_index
+
+		assert (self.member.x >= lower_limit and self.member.x <= upper_limit), 'Initialized X Value is outside the bounds of the view area'
+		assert (self.member.y >= lower_limit and self.member.y <= upper_limit), 'Initialized Y Value is outside the bounds of the view area'
+		assert (color_pass == True and color_index == None), 'Color in position %i is outside accepted RGB values' % color_index
+		assert (self.member.brain is not None), 'Brain was not added to the member class'
 
 	def tearDown( self ):
 		self.member = None
