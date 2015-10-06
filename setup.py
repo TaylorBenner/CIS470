6aa25818fbb50a2e98933c4341feec1a2aafd33f
Taylor Benner <taylor.benner@madwiremedia.com>
2015-10-05 21:05:42 -0600

    change setup

--- setup.py
+++ setup.py
@@ -2,4 +2,11 @@ import numpy
 from distutils.core import setup
 import py2exe
 
-setup(console=['project.py'])
+setup(
+	options = {
+        "py2exe": {
+            "dll_excludes": ["MSVCP90.dll"]
+        }
+    },
+	console=['project.py']
+)
